497d9913396e6c7f627f613ad983c169bc98b354
VuTraMy66 <153701476+trmzaiu@users.noreply.github.com>
2025-03-07 19:55:59 +0700

    update camera & scan

--- lib/theme/fonts.dart
+++ lib/theme/fonts.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+<<<<<<< Updated upstream
 class AppFonts {
   static const String primaryFont = 'Nunito';
   static const String secondaryFont = 'Domine';
@@ -16,4 +17,15 @@ class AppFonts {
     fontSize: 16,
     color: Colors.black87,
   );
+=======
+class AppFontWeight {
+  static const FontWeight thin = FontWeight.w200;        // Thin
+  static const FontWeight extraLight = FontWeight.w300;  // Extra Light (Ultra Light)
+  static const FontWeight light = FontWeight.w400;       // Light
+  static const FontWeight regular = FontWeight.w500;     // Regular (Normal)
+  static const FontWeight medium = FontWeight.w600;      // Medium
+  static const FontWeight semiBold = FontWeight.w700;    // SemiBold (DemiBold)
+  static const FontWeight bold = FontWeight.w800;        // Bold
+  static const FontWeight extraBold = FontWeight.w900;   // Extra Bold (Ultra Bold)
+>>>>>>> Stashed changes
 }
