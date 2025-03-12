54ba45568a5b7755a81bcbc1ba6301df265bcc86
VuTraMy66 <153701476+trmzaiu@users.noreply.github.com>
2025-03-12 21:36:57 +0700

    Revert "commit"

--- lib/frontend/screen/camera/camera_screen.dart
+++ lib/frontend/screen/camera/camera_screen.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:camerawesome/camerawesome_plugin.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:camerawesome/src/widgets/camera_awesome_builder.dart';
+import 'package:camerawesome/src/widgets/utils/awesome_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:image_picker/image_picker.dart';
