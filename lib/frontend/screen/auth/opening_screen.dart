68aef635e2340ce33d0b4f752056dfa96326eba5
VuTraMy66 <153701476+trmzaiu@users.noreply.github.com>
2025-03-15 01:02:19 +0700

    commit

--- lib/frontend/screen/auth/opening_screen.dart
+++ lib/frontend/screen/auth/opening_screen.dart
@@ -3,85 +3,128 @@ import 'package:wastesortapp/theme/colors.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:wastesortapp/frontend/screen/auth/login_screen.dart';
 
+import '../../../theme/fonts.dart';
 import 'login_screen.dart';
 
-class OpeningScreen extends StatelessWidget {
-  const OpeningScreen({super.key});
+class OpeningScreen extends StatefulWidget {
+  const OpeningScreen({Key? key}) : super(key: key);
+
+  @override
+  _OpeningScreenState createState() => _OpeningScreenState();
+}
+
+class _OpeningScreenState extends State<OpeningScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _navigateToLogin() async {
+    await Future.delayed(Duration(milliseconds: 100));
+
+    if (mounted) {
+      Navigator.of(context).pushReplacement(
+        PageRouteBuilder(
+          transitionDuration: Duration(milliseconds: 600),
+          pageBuilder: (_, __, ___) => LoginScreen(),
+          transitionsBuilder: (_, animation, __, child) {
+            const begin = Offset(0.0, 1.0);
+            const end = Offset.zero;
+            const curve = Curves.easeInOut;
+
+            var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+            return SlideTransition(
+              position: animation.drive(tween),
+              child: child,
+            );
+          },
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Waste Sorting Image
-            Expanded(
-              child: Center(
-                child: Image.asset(
-                  "lib/assets/images/opening.png",
-                  width: 387.12,
-                ),
+      body: Column(
+        children: [
+          SizedBox(height: 120),
+          // Waste Sorting Image
+          Center(
+            child: RotationTransition(
+              turns: _controller,
+              child: Image.asset(
+                "lib/assets/images/opening.png",
+                height: 380,
               ),
             ),
+          ),
 
-            // Welcome Text
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Hello.",
-                    style: GoogleFonts.urbanist(
-                        fontSize: 36,
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.secondary),
-                  ),
-                  Text(
-                    "Welcome back!",
-                    style: GoogleFonts.urbanist(
-                        fontSize: 36,
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.secondary),
-                  ),
-                ],
-              ),
-            ),
-
-            const SizedBox(height: 20),
-
-            // "Getting Started" Button
-            GestureDetector(
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginScreen()),
-                );
-              },
-              child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 30),
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: AppColors.primary,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                alignment: Alignment.center,
-                child: Text(
-                  "Getting started",
+          // Welcome Text
+          SizedBox(
+            width: 330,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Hello.",
                   style: GoogleFonts.urbanist(
-                      color: AppColors.surface,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold),
+                      fontSize: 36,
+                      fontWeight: AppFontWeight.semiBold,
+                      color: AppColors.secondary
+                  ),
+                ),
+                Text(
+                  "Welcome back!",
+                  style: GoogleFonts.urbanist(
+                      fontSize: 36,
+                      fontWeight: AppFontWeight.semiBold,
+                      color: AppColors.secondary
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          SizedBox(height: 60),
+
+          // "Getting Started" Button
+          GestureDetector(
+            onTap: _navigateToLogin,
+            child: Container(
+              width: 330,
+              height: 55,
+              decoration: BoxDecoration(
+                color: AppColors.primary,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                "Getting started",
+                style: GoogleFonts.urbanist(
+                  color: AppColors.surface,
+                  fontSize: 18,
+                  fontWeight: AppFontWeight.semiBold
                 ),
               ),
             ),
-
-            const SizedBox(height: 50),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
